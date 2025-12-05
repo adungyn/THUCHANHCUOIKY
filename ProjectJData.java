de300b9fba8f7903d87d3bbdd42aaa20879b863c
adungyn <adu.ngyn@gmail.com>
2025-12-05 21:07:28 +0700

    Update ProjectJData.java

--- ProjectJData.java
+++ ProjectJData.java
@@ -1,23 +1,16 @@
-/**
- * ProjectData.java
- * Lớp này mô phỏng cấu trúc dữ liệu cho dự án Nhím Tập Gieo.
- * Nó có thể được sử dụng trong môi trường backend (server) để quản lý
- * thông tin chi tiết về các mùa dự án, thành viên, và giáo án.
- */
+
 public class ProjectData {
 
     private String projectName;
     private String projectPhase;
     private int totalMembers;
 
-    // Constructor
     public ProjectData(String projectName, String projectPhase, int totalMembers) {
         this.projectName = projectName;
         this.projectPhase = projectPhase;
         this.totalMembers = totalMembers;
     }
 
-    // Phương thức Getter
     public String getProjectName() {
         return projectName;
     }
@@ -30,7 +23,6 @@ public class ProjectData {
         return totalMembers;
     }
 
-    // Phương thức mô phỏng in thông tin
     public void printProjectSummary() {
         System.out.println("--- Tổng quan Dự án ---");
         System.out.println("Tên dự án: " + projectName);
@@ -38,7 +30,6 @@ public class ProjectData {
         System.out.println("Số thành viên: " + totalMembers);
     }
 
-    // Phương thức Main để chạy thử (Test)
     public static void main(String[] args) {
         ProjectData nhimTapGieo = new ProjectData("Nhím Tập Gieo", "Mùa 2: STEM", 26);
         nhimTapGieo.printProjectSummary();
